e3fadeb052ad1242a9018533ed267f5aafe1ca14
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-26 16:52:34 +0500

    create send function

--- contracts/Guardian.sol
+++ contracts/Guardian.sol
@@ -4,9 +4,23 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+error Guardian__InvalidAmount(uint amount);
+error Guardian__TransactionFailed();
+
 contract Guardian is Ownable {
     // * FUNCTIONS
     fallback() external payable {}
 
     receive() external payable {}
+
+    function send(address to, uint amount) external {
+        if (amount <= 0) {
+            revert Guardian__InvalidAmount(amount);
+        }
+
+        (bool success, ) = to.call{value: amount}("");
+        if (!success) {
+            revert Guardian__TransactionFailed();
+        }
+    }
 }
