9a5773bda2d13b6d87762501cc82caae7736f3a6
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-26 16:32:32 +0500

    add fallback and receive functions

--- contracts/Guardian.sol
+++ contracts/Guardian.sol
@@ -4,4 +4,9 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Guardian is Ownable {}
+contract Guardian is Ownable {
+    // * FUNCTIONS
+    fallback() external payable {}
+
+    receive() external payable {}
+}
