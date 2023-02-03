ed03eb2e1355e3e9d6f5f2044672cf23a2f84064
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-03 15:27:58 +0500

    add gasReporter object in hardhat.config.ts file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
@@ -9,6 +10,13 @@ const config: HardhatUserConfig = {
         deployer: {
             default: 0,
         },
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
 };
 
