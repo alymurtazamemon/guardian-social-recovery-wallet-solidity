986dc8d17ccbe1f7be2d4439f9b6c82d5e1b0f2f
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-03 15:28:26 +0500

    add networks object in hardhat.config.ts file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,20 @@ import "dotenv/config";
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
     defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            chainId: 5,
+            url: process.env.GOERLI_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY!],
+            saveDeployments: true,
+        },
+    },
     namedAccounts: {
         deployer: {
             default: 0,
