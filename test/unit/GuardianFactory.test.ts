e293d69284461eda4a1ef37e4f728ab88e0e1d88
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-08 23:44:22 +0500

    write test - getContractAddressByGuardian - should revert if guardian does not exist.

--- test/unit/GuardianFactory.test.ts
+++ test/unit/GuardianFactory.test.ts
@@ -137,5 +137,62 @@ import { expect } from "chai";
                       "GuardianFactory__OwnerContractNotFound"
                   );
               });
+
+              describe("getContractAddressByGuardian - After Creating Wallet", () => {
+                  beforeEach(async () => {
+                      const [_, account2, account3, account4, account5] =
+                          await ethers.getSigners();
+
+                      const tx: ContractTransaction =
+                          await guardiansFactroy.createWallet();
+
+                      await tx.wait(1);
+
+                      const walletAddress: string =
+                          await guardiansFactroy.getWallet();
+
+                      guardian = await ethers.getContractAt(
+                          "Guardian",
+                          walletAddress
+                      );
+
+                      const newGuardians: string[] = [
+                          account2.address,
+                          account3.address,
+                          account4.address,
+                      ];
+
+                      for (let i = 0; i < newGuardians.length; i++) {
+                          const addTime: BigNumber =
+                              await guardian.getLastGuardianAddTime();
+
+                          const delay: BigNumber =
+                              await guardian.getAddGuardianDelay();
+
+                          await network.provider.send("evm_increaseTime", [
+                              addTime.toNumber() + delay.toNumber(),
+                          ]);
+
+                          const tx = await guardian.addGuardian(
+                              newGuardians[i]
+                          );
+                          await tx.wait(1);
+                      }
+                  });
+
+                  it("should revert if guardian does not exist.", async () => {
+                      const [_, account2, account3, account4, account5] =
+                          await ethers.getSigners();
+
+                      await expect(
+                          guardiansFactroy
+                              .connect(account5)
+                              .getContractAddressByGuardian(_.address)
+                      ).to.be.revertedWithCustomError(
+                          guardiansFactroy,
+                          "GuardianFactory__AddressNotFoundAsGuardian"
+                      ).withArgs(account5.address);
+                  });
+              });
           });
       });
