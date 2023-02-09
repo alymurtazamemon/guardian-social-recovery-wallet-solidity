3576242344b4210c305528067f13292e21767d8b
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-09 19:12:16 +0500

    write Natspec for Guardian contract

--- contracts/Guardian.sol
+++ contracts/Guardian.sol
@@ -5,6 +5,12 @@ pragma solidity ^0.8.17;
 import "./FundsManager.sol";
 import "./OwnershipManager.sol";
 
+/**
+ * @title Guardian - A Social Recovery Wallet.
+ * @author Ali Murtaza Memon
+ * @notice This wallet has additional features compared to a regular wallet, which can prevent the loss of ownership in the event of a private key loss and protect funds from theft.
+ * @dev This contract takes Chainlink's Goerli ETH/USD price feed address and GuardianFactory contract address in the constructor.
+ */
 contract Guardian is FundsManager, OwnershipManager {
     constructor(
         address priceFeed,
